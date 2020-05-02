6e038ec9b910d340d4627ce9b667c55b4a6ba647
MusaTamzid05 <tamzid101@gmail.com>
2020-05-03 05:30:21 +0600

    show_image from util deleted.

--- include/util.h
+++ include/util.h
@@ -4,10 +4,6 @@
 
 
 namespace Util {
-
-
-    void show_image(const std::string& image_path, const std::string window_name = "Window" , bool delay = true);
-    void show_image(const cv::Mat& image , const std::string window_name = "Window" , bool delay = true);
     bool should_resize(const cv::Mat& image);
 };
 
--- src/util.cpp
+++ src/util.cpp
@@ -4,30 +4,6 @@
 
 namespace Util {
 
-    void show_image(const std::string& image_path, const std::string window_name , bool delay ) {
-
-        cv::Mat image = cv::imread(image_path);
-
-        if(image.empty()) {
-            std::cerr << "Could not load " << image_path << "\n";
-            return;
-        }
-
-        show_image(image , window_name , delay);
-
-    }
-
-
-    void show_image(const cv::Mat& image , const std::string window_name  , bool delay) {
-
-        cv::namedWindow(window_name , cv::WINDOW_NORMAL);
-        cv::imshow(window_name , image);
-
-        if(delay)
-            cv::waitKey(0);
-    }
-
-
     bool should_resize(const cv::Mat& image) {
         return image.size().width > Const::WIDTH || image.size().height > Const::HEIGHT;
     }
