68b49120c9b6e25997cc3697c840e7b28ad385b8
MusaTamzid05 <tamzid101@gmail.com>
2020-05-03 07:06:29 +0600

    command line argument added.

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,14 @@
 
 int main(int argc , char** argv) {
 
-    App::Labeler labeler("../test.jpg");
-    labeler.run("./results");
+    if(argc != 3) {
+        std::cerr << "Usage : image_path result_dir\n";
+        return -1;
+    }
+
+    
+    App::Labeler labeler(argv[1]);
+    labeler.run(argv[2]);
     
 
     return 0;
